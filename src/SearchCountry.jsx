c110f677a04ba9511000640afdecaaacb416bf02
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-11-06 15:25:19 +0100

    started population project

---
+++ src/SearchCountry.jsx
@@ -0,0 +1,21 @@
+export default function SearchBar({ setCountry, setName, country }) {
+  return (
+    <div className="w-[400px] h-[50px] absolute top-2 left-[35%] flex justify-between">
+      <input
+        className="w-[70%] "
+        type="text"
+        onChange={(e) => {
+          setCountry(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setName(country);
+        }}
+        className="w-[20%]  bg-red-900 hover:bg-red-600"
+      >
+        Search
+      </button>
+    </div>
+  );
+}
--- src/landingPage/langind.jsx
+++ src/landingPage/langind.jsx
@@ -2,16 +2,18 @@ import axios from "axios";
 import { useRef } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import SearchBar from "../SearchCountry";
 
 export default function Langing() {
   const [countries, setCountries] = useState([]);
   const [points, setPoints] = useState(0);
-  const ref = useRef(false);
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((res) => {
-      setCountries(res.data);
-    });
-  }, []);
+  const [show, setShow] = useState(false);
+  const [mapCounteries, setMapCounteries] = useState([]);
+
+  const [country, setCountry] = useState("");
+  const [name, setName] = useState("serbia");
+
+  // https://countryflagsapi.com/png/ link za slike
 
   function numFormatter(num) {
     if (num > 999 && num < 1000000) {
@@ -22,88 +24,33 @@ export default function Langing() {
       return num; // if value < 1000, nothing to do
     }
   }
-
-  function randomCountry() {
-    while (countries.length !== 0) {
-      let randomCounteries = Array(2)
-        .fill()
-        .map(() => {
-          return countries[Math.floor(Math.random() * 200)];
-        });
-
-      return randomCounteries;
-    }
-  }
-  function CorrectAnswer(value, country) {
-    if (value === "higher") {
-      if (country[1].population > country[0].population) {
-        setPoints(points + 1);
-      } else {
-        setPoints(0);
-      }
-    } else {
-      if (country[1].population < country[0].population) {
-        setPoints(points + 1);
-      } else {
-        setPoints(0);
-      }
-    }
-  }
-  function CorrectAnswerFinal(value, country) {
-    ref.current = !ref.current;
-    setTimeout(() => {
-      CorrectAnswer(value, country);
-      ref.current = false;
-    }, 1000);
-  }
-  let arr = [];
+  useEffect(() => {
+    // axios
+    //   .get("https://countriesnow.space/api/v0.1/countries/population", {
+    //     params: { country: "nigeria" },
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
+    axios({
+      method: "post",
+      url: "https://countriesnow.space/api/v0.1/countries/population",
+      data: {
+        country: `${name}`,
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+  }, [name]);
+  let population = 200;
+  console.log(name);
   return (
-    <div className="flex flex-wrap content-center justify-center">
-      <h1 className="absolute top-0 text-red-500 text-3xl">{points}</h1>
-      {randomCountry()?.map((country, index) => {
-        arr.push(country);
-        return (
-          <div className="w-[700px] h-[700px] bg-blue-500 m-2 p-2 border gap-x-2.5">
-            <h1 className="text-3xl font-bold text-white">
-              {country.name.common}
-            </h1>
-            <img
-              className="h-[50%] w-screen shadow-xl"
-              src={`${country.flags.png}`}
-              alt=""
-            />
-            {ref.current ? (
-              <h1 className="text-2xl text-white ">
-                {numFormatter(parseInt(country.population))}
-              </h1>
-            ) : (
-              ""
-            )}
-            {index === 1 ? (
-              <div className="relative w-full lg:max-w-sm">
-                <button
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={(el) => {
-                    CorrectAnswerFinal(el.target.innerText, arr);
-                  }}
-                >
-                  higher
-                </button>
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={(el) => {
-                    CorrectAnswerFinal(el.target.innerText, arr);
-                  }}
-                >
-                  lower
-                </button>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <SearchBar country={country} setCountry={setCountry} setName={setName} />
+      <div className="bg-yellow-500 h-screen flex justify-center items-center">
+        <div className="w-96 h-[200px] bg-black"> nes</div>
+        <div className={`bg-blue-400 w-96 h-[${population}px]`}>nelo</div>
+      </div>
+    </>
   );
 }
