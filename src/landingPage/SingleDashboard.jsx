20dc4974b8f9b55f0056f160118f2de2171d0b7a
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-11-09 01:18:35 +0100

    makin logic for dynamic values of counteries with diffrence in digits counteries

--- src/landingPage/SingleDashboard.jsx
+++ src/landingPage/SingleDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import "./cactus.css";
-export default function Cactus({ pop, overall }) {
+export default function Cactus({ pop, overall, arr }) {
   function numFormatter(num) {
     if (num > 999 && num < 1000000) {
       return num / (1000).toFixed(1); // convert to K for number from > 1000 < 1 million
@@ -11,40 +11,58 @@ export default function Cactus({ pop, overall }) {
       return num; // if value < 1000, nothing to do
     }
   }
+  console.log(Math.max(...arr), Math.min(...arr));
 
   function formatPopulationForHeight(pop) {
-    if (pop > 999 && pop < 10000) {
-      return pop / 10;
-    } else if (pop > 9999 && pop < 100000) {
-      return pop / 100;
-    } else if (pop > 99999 && pop < 1000000) {
-      return pop / 1000;
-    } else if (pop > 999999 && pop < 10000000) {
-      return pop / 10000;
-    } else if (pop > 9999999 && pop < 100000000) {
-      return pop / 100000;
-    } else if (pop > 99999999 && pop < 1000000000) {
-      return pop / 1000000;
-    } else if (pop > 999999999 && pop < 10000000000) {
-      return pop / 10000000;
+    if (
+      Math.max(...arr).toString().length > Math.min(...arr).toString().length
+    ) {
+    }
+    if (pop > 999 && pop < 100000) {
+      return { pop: pop / 100, color: "#41F253" };
+    } else if (pop > 99999 && pop < 10000000) {
+      return { pop: pop / 10000, color: "#469d50" };
+    } else if (pop > 9999999 && pop < 10000000000) {
+      return { pop: pop / 1000000, color: "#25642d" };
     }
   }
-  console.log();
 
   return (
-    <div className="m-[50px]">
-      {console.log(formatPopulationForHeight(overall), "in funck")}
+    <div
+      className="m-[50px]"
+      style={{
+        height: `${
+          formatPopulationForHeight(overall).pop / 10 > 80
+            ? formatPopulationForHeight(pop.value).pop / 10 - 20
+            : formatPopulationForHeight(pop.value).pop / 10
+        }%`,
+      }}
+    >
       <div
         style={{
-          height:
-            formatPopulationForHeight(overall) < 500
-              ? formatPopulationForHeight(pop.value) + 200
-              : formatPopulationForHeight(pop.value),
+          background: formatPopulationForHeight(pop.value).color,
+          height: "100%",
         }}
         className="cactus-body"
       >
-        <div className="arm left"></div>
-        <div className="arm right"></div>
+        <div
+          style={{ background: formatPopulationForHeight(pop.value).color }}
+          className="arm left"
+        >
+          <div
+            style={{ background: formatPopulationForHeight(pop.value).color }}
+            className="leftt"
+          ></div>
+        </div>
+        <div
+          className="arm right"
+          style={{ background: formatPopulationForHeight(pop.value).color }}
+        >
+          <div
+            style={{ background: formatPopulationForHeight(pop.value).color }}
+            className="rightt"
+          ></div>
+        </div>
       </div>
     </div>
   );
--- src/landingPage/langind.jsx
+++ src/landingPage/langind.jsx
@@ -9,9 +9,11 @@ export default function Langing() {
   const [name, setName] = useState("serbia");
   const [population, setPopulation] = useState([]);
   const [overall, setOverall] = useState(0);
+  const [arrForPop, setArrForPop] = useState([]);
 
   // https://countryflagsapi.com/png/ link za slike
   let helper = 0;
+  let arr = [];
   useEffect(() => {
     // axios
     //   .get("https://countriesnow.space/api/v0.1/countries/population", {
@@ -30,9 +32,12 @@ export default function Langing() {
     }).then((response) => {
       setPopulation(response.data.data.populationCounts);
       for (let i of response.data.data.populationCounts) {
+        arr.push(i.value);
+
         helper += i.value;
         setOverall(helper / 29);
       }
+      setArrForPop(arr);
     });
   }, [name]);
   console.log("OVERALL this neam", overall);
@@ -40,9 +45,9 @@ export default function Langing() {
     <>
       <SearchBar country={country} setCountry={setCountry} setName={setName} />
       <div className="bg-yellow-500 h-screen flex justify-center items-end">
-        <div className="bg-amber-600 flex  items-end min-h-[80%] min-w-[80%] rounded-2xl overflow-scroll">
+        <div className="bg-amber-600 flex m-[25px]  items-end h-[500px] min-w-[80%] rounded-2xl overflow-scroll">
           {population.map((pop) => {
-            return <Cactus pop={pop} overall={overall} />;
+            return <Cactus pop={pop} overall={overall} arr={arrForPop} />;
           })}
         </div>
       </div>
